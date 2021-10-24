debd6478bed8765e839ddc9a1489a4342e94809b
saprykins <palatatot@gmail.com>
2021-10-24 11:39:28 +0200

    pseudo code

---
+++ app.py
@@ -0,0 +1,22 @@
+# import ...
+
+# Controller
+    # initialization of WSGI application
+    
+    # routing
+        # when we upload a pdf, it responds with a document ID
+        # @app.route("/documents")
+        
+    # application execution
+
+# Model
+    # extracting metadata from pdf-file
+    # extracting text from pdf-file
+
+# View
+    # describes document processing state, metadata and shares a link to its content
+    # @app.route("/documents/<id>")
+
+    # displays the extracted text
+    # @app.route("/text/<id>.txt")
+
