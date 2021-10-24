6be4deb6c146f31d7aeedfcb004977d660d840b7
saprykins <palatatot@gmail.com>
2021-10-24 17:23:14 +0200

    allows to upload files via curl-command to local folder

--- app.py
+++ app.py
@@ -1,22 +1,52 @@
-# import ...
+from flask import Flask
+from flask import render_template
+from flask import request
+from werkzeug.utils import secure_filename
 
-# Controller
-    # initialization of WSGI application
+
+
+# CONTROLLER
+
+# initialization of WSGI application
+app = Flask(__name__)
+
+# routing to index page
+@app.route("/")
+def index():
+    return str("Index page")
+
+# routing to the endpoint that allows file upload
+@app.route('/documents', methods=['POST'])
+def upload_file():
+    """
+    It saves the file in "local_file_path" received in curl command 
+    and returns file's name
     
-    # routing
-        # when we upload a pdf, it responds with a document ID
-        # @app.route("/documents")
+    An example of curl command to upload a file from command line:
+    curl -F file=@"f.txt" http://localhost:5000/documents
+    """
+    local_file_path = '../DOWNLOADS/uploaded_file-3.txt'
+    file = request.files['file']
+    file.save(local_file_path)
+    filename=secure_filename(file.filename)   
+    return filename
+
         
-    # application execution
+if __name__ == '__main__':
+    app.run(debug=True)
 
-# Model
-    # extracting metadata from pdf-file
-    # extracting text from pdf-file
 
-# View
-    # describes document processing state, metadata and shares a link to its content
-    # @app.route("/documents/<id>")
+# MODEL
 
-    # displays the extracted text
-    # @app.route("/text/<id>.txt")
+# extracting metadata from pdf-file
+# extracting text from pdf-file
+
+
+# VIEW
+
+# describes document processing state, metadata and shares a link to its content
+# @app.route("/documents/<id>")
+
+# displays the extracted text
+# @app.route("/text/<id>.txt")
 
