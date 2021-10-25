ca374a09dc60ce3fac6e72a97394ed3213b6854c
saprykins <palatatot@gmail.com>
2021-10-25 22:22:24 +0200

    endpoint to display text is added

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ from werkzeug.utils import secure_filename
 # 1/ must return file_id, not filename
 # 2/ to test in virtual environment
 
+# requirements related to view are to accomplish:
+# 1/ prints out all the text, not only the first line
+# 2/ must find the file by id, not its name
+
 # possible improuvements:
 # 1/ check file extention before upload
 # 2/ delete "debug=True" in main function
@@ -20,10 +24,12 @@ from werkzeug.utils import secure_filename
 # initialization of WSGI application
 app = Flask(__name__)
 
+
 # routing to index page
 @app.route("/")
 def index():
     return str("Index page")
+
 
 # routing to the endpoint that allows file upload
 @app.route('/documents', methods=['POST'])
@@ -41,9 +47,26 @@ def upload_file():
     filename=secure_filename(file.filename)   
     return filename
 
-        
-if __name__ == '__main__':
-    app.run(debug=True)
+
+
+# VIEW
+
+# describes document processing state, metadata and shares a link to its content
+# @app.route("/documents/<id>")
+
+
+# displays the extracted text
+@app.route('/text/<id>.txt', methods=['GET', 'POST'])
+def print_text(id):
+    file_path = '../DOWNLOADS/'
+    # id = 'uploaded_file-3'
+    file_name = id
+    file_path = file_path + file_name + '.txt'
+    
+    with open(file_path) as feed:
+        for line in feed:
+            # returns only the first line
+            return str(line.strip())
 
 
 
@@ -53,12 +76,11 @@ if __name__ == '__main__':
 # extracting text from pdf-file
 
 
+if __name__ == '__main__':
+    app.run(debug=True)
+    # app.run()
 
-# VIEW
 
-# describes document processing state, metadata and shares a link to its content
-# @app.route("/documents/<id>")
 
-# displays the extracted text
-# @app.route("/text/<id>.txt")
+
 
